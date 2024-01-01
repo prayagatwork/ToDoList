423cd5068004286c9484e949ffb762b7f62541e2
PRAYAG THAKER <prayagthaker@PRAYAGs-MacBook-Pro.local>
2024-01-01 18:53:32 +0530

    all set for todo launch

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+app.listen(port, function(){
     console.log("server is up and live");
 });
 // mongoose.connect('mongodb://localhost:27017/todoDB');
